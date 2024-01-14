1f187648be5d0245a0f5e5cc474e34ecb6c4a5da
Alexey Furs <wiemrid@gmail.com>
2024-01-15 00:48:42 +0300

    update migration and db seed

---
+++ backend/src/database/migrations/1705262120844-migration.ts
@@ -0,0 +1,127 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1705262120844 implements MigrationInterface {
+    name = 'Migration1705262120844'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TYPE "public"."roles_name_enum" AS ENUM('Admin', 'Editor', 'User')
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "roles" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "deleted_at" TIMESTAMP,
+                "name" "public"."roles_name_enum" NOT NULL,
+                CONSTRAINT "UQ_648e3f5447f725579d7d4ffdfb7" UNIQUE ("name"),
+                CONSTRAINT "PK_c1433d71a4838793a49dcad46ab" PRIMARY KEY ("id")
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "users" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "deleted_at" TIMESTAMP,
+                "email" character varying NOT NULL,
+                "password" character varying NOT NULL,
+                CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"),
+                CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "tours" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "deleted_at" TIMESTAMP,
+                "name" character varying NOT NULL,
+                "starting_date" TIMESTAMP WITH TIME ZONE NOT NULL,
+                "ending_date" TIMESTAMP WITH TIME ZONE NOT NULL,
+                "price" integer NOT NULL,
+                "travel_id" uuid,
+                CONSTRAINT "uq_travel_id_name" UNIQUE ("name", "travel_id"),
+                CONSTRAINT "PK_2202ba445792c1ad0edf2de8de2" PRIMARY KEY ("id")
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "travels" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "deleted_at" TIMESTAMP,
+                "is_public" boolean NOT NULL DEFAULT true,
+                "name" character varying NOT NULL,
+                "slug" character varying NOT NULL,
+                "description" character varying NOT NULL,
+                "number_of_days" integer NOT NULL,
+                "moods" jsonb NOT NULL,
+                CONSTRAINT "UQ_b24d3561bfa27a076dd9466cd28" UNIQUE ("name"),
+                CONSTRAINT "PK_cc2d44f93ba8f6b268978971e2b" PRIMARY KEY ("id")
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "users_roles" (
+                "user_id" uuid NOT NULL,
+                "role_id" uuid NOT NULL,
+                CONSTRAINT "PK_c525e9373d63035b9919e578a9c" PRIMARY KEY ("user_id", "role_id")
+            )
+        `);
+        await queryRunner.query(`
+            CREATE INDEX "IDX_e4435209df12bc1f001e536017" ON "users_roles" ("user_id")
+        `);
+        await queryRunner.query(`
+            CREATE INDEX "IDX_1cf664021f00b9cc1ff95e17de" ON "users_roles" ("role_id")
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "tours"
+            ADD CONSTRAINT "FK_b3977b16c820f3a64680038c54e" FOREIGN KEY ("travel_id") REFERENCES "travels"("id") ON DELETE CASCADE ON UPDATE NO ACTION
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "users_roles"
+            ADD CONSTRAINT "FK_e4435209df12bc1f001e5360174" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "users_roles"
+            ADD CONSTRAINT "FK_1cf664021f00b9cc1ff95e17de4" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE CASCADE ON UPDATE CASCADE
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE "users_roles" DROP CONSTRAINT "FK_1cf664021f00b9cc1ff95e17de4"
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "users_roles" DROP CONSTRAINT "FK_e4435209df12bc1f001e5360174"
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "tours" DROP CONSTRAINT "FK_b3977b16c820f3a64680038c54e"
+        `);
+        await queryRunner.query(`
+            DROP INDEX "public"."IDX_1cf664021f00b9cc1ff95e17de"
+        `);
+        await queryRunner.query(`
+            DROP INDEX "public"."IDX_e4435209df12bc1f001e536017"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "users_roles"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "travels"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "tours"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "users"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "roles"
+        `);
+        await queryRunner.query(`
+            DROP TYPE "public"."roles_name_enum"
+        `);
+    }
+
+}
--- backend/src/database/seeders/seed/seed.service.ts
+++ backend/src/database/seeders/seed/seed.service.ts
@@ -5,8 +5,93 @@ import { InjectDataSource } from '@nestjs/typeorm';
 import { UserRole } from '../../../roles/constants';
 import { Role } from '../../../roles/entities';
 import { User } from '../../../users/entities';
-import { HashService } from '../../../utils/hash/hash.service';
+import { HashService } from '../../../utils/hash';
 import { Travel } from '../../../travels/entities';
+import { Tour } from '../../../tours/entities';
+
+const exampleTravels = [
+  {
+    name: 'Jordan 360°',
+    description:
+      'Jordan 360°: the perfect tour to discover the suggestive Wadi Rum desert, the ancient beauty of Petra, and much more.\n\nVisiting Jordan is one of the most fascinating things that everyone has to do once in their life.You are probably wondering "Why?". Well, that\'s easy: because this country keeps several passions! During our tour in Jordan, you can range from well-preserved archaeological masterpieces to trekkings, from natural wonders excursions to ancient historical sites, from camels trek in the desert to some time to relax.\nDo not forget to float in the Dead Sea and enjoy mineral-rich mud baths, it\'s one of the most peculiar attractions. It will be a tour like no other: this beautiful country leaves a memorable impression on everyone.',
+    numberOfDays: 8,
+    moods: {
+      nature: 80,
+      relax: 20,
+      history: 90,
+      culture: 30,
+      party: 10,
+    },
+    tours: [
+      {
+        name: 'ITJOR20211101',
+        startingDate: new Date('2021-11-01'),
+        endingDate: new Date('2021-11-09'),
+        price: 1999,
+      },
+      {
+        name: 'ITJOR20211112',
+        startingDate: new Date('2021-11-12'),
+        endingDate: new Date('2021-11-20'),
+        price: 1899,
+      }
+    ]
+  },
+  {
+    name: 'Iceland: hunting for the Northern Lights',
+    description:
+      "Why visit Iceland in winter? Because it is between October and March that this land offers the spectacle of the Northern Lights, one of the most incredible and magical natural phenomena in the world, visible only near the earth's two magnetic poles. Come with us on WeRoad to explore this land of ice and fire, full of contrasts and natural variety, where the energy of waterfalls and geysers meets the peace of the fjords... And when the ribbons of light of the aurora borealis twinkle in the sky before our enchanted eyes, we will know that we have found what we were looking for.",
+    numberOfDays: 8,
+    moods: {
+      nature: 100,
+      relax: 30,
+      history: 10,
+      culture: 20,
+      party: 10,
+    },
+    tours: [
+      {
+        name: 'ITJOR20211125',
+        startingDate: new Date('2021-11-25'),
+        endingDate: new Date('2021-12-03'),
+        price: 2149,
+      },
+      {
+        name: 'ITICE20211101',
+        startingDate: new Date('2021-11-01'),
+        endingDate: new Date('2021-11-08'),
+        price: 1999,
+      }
+    ]
+  },
+  {
+    name: 'United Arab Emirates: from Dubai to Abu Dhabi',
+    description:
+      'At Dubai and Abu Dhabi everything is huge and majestic: here futuristic engineering works and modern infrastructures meet historical districts, local souks (typical bazars), desert and the sea. In this tour we’ll discover Dubai, where we’ll get on top of the highest skyscraper ever built, the Burj Khalifa. We’ll then take a walk at the Dubai Mall, the biggest mall on the planet, and we’ll spend a night in the desert, with the sight of the skyline on the horizon keeping us company. Then, it will be Abu Dhabi’s tourn! Sheik Zayed’s Grand Mosque’s white marble awaits for us to remain stoked in front of its wonderfulness, and the sea will invade us with peacefulness. UAE are all to discover, we’ll just get a taste of it, but we guarantee you’ll get quite the idea!',
+    numberOfDays: 7,
+    moods: {
+      nature: 30,
+      relax: 40,
+      history: 20,
+      culture: 80,
+      party: 70,
+    },
+    tours: [
+      {
+        name: 'ITARA20211221',
+        startingDate: new Date('2021-12-21'),
+        endingDate: new Date('2021-12-28'),
+        price: 1899,
+      },
+      {
+        name: 'ITARA20211222',
+        startingDate: new Date('2022-01-03'),
+        endingDate: new Date('2022-01-10'),
+        price: 1499,
+      }
+    ]
+  },
+];
 
 @Injectable()
 export class SeedService {
@@ -79,56 +164,18 @@ export class SeedService {
   }
 
   async createTravels(): Promise<void> {
-    const exampleTravels = [
-      {
-        name: 'Jordan 360°',
-        description:
-          'Jordan 360°: the perfect tour to discover the suggestive Wadi Rum desert, the ancient beauty of Petra, and much more.\n\nVisiting Jordan is one of the most fascinating things that everyone has to do once in their life.You are probably wondering "Why?". Well, that\'s easy: because this country keeps several passions! During our tour in Jordan, you can range from well-preserved archaeological masterpieces to trekkings, from natural wonders excursions to ancient historical sites, from camels trek in the desert to some time to relax.\nDo not forget to float in the Dead Sea and enjoy mineral-rich mud baths, it\'s one of the most peculiar attractions. It will be a tour like no other: this beautiful country leaves a memorable impression on everyone.',
-        numberOfDays: 8,
-        moods: {
-          nature: 80,
-          relax: 20,
-          history: 90,
-          culture: 30,
-          party: 10,
-        },
-      },
-      {
-        name: 'Iceland: hunting for the Northern Lights',
-        description:
-          "Why visit Iceland in winter? Because it is between October and March that this land offers the spectacle of the Northern Lights, one of the most incredible and magical natural phenomena in the world, visible only near the earth's two magnetic poles. Come with us on WeRoad to explore this land of ice and fire, full of contrasts and natural variety, where the energy of waterfalls and geysers meets the peace of the fjords... And when the ribbons of light of the aurora borealis twinkle in the sky before our enchanted eyes, we will know that we have found what we were looking for.",
-        numberOfDays: 8,
-        moods: {
-          nature: 100,
-          relax: 30,
-          history: 10,
-          culture: 20,
-          party: 10,
-        },
-      },
-      {
-        name: 'United Arab Emirates: from Dubai to Abu Dhabi',
-        description:
-          'At Dubai and Abu Dhabi everything is huge and majestic: here futuristic engineering works and modern infrastructures meet historical districts, local souks (typical bazars), desert and the sea. In this tour we’ll discover Dubai, where we’ll get on top of the highest skyscraper ever built, the Burj Khalifa. We’ll then take a walk at the Dubai Mall, the biggest mall on the planet, and we’ll spend a night in the desert, with the sight of the skyline on the horizon keeping us company. Then, it will be Abu Dhabi’s tourn! Sheik Zayed’s Grand Mosque’s white marble awaits for us to remain stoked in front of its wonderfulness, and the sea will invade us with peacefulness. UAE are all to discover, we’ll just get a taste of it, but we guarantee you’ll get quite the idea!',
-        numberOfDays: 7,
-        moods: {
-          nature: 30,
-          relax: 40,
-          history: 20,
-          culture: 80,
-          party: 70,
-        },
-      },
-    ];
-
     const travelsRepository = this.defaultDataSource.getRepository(Travel);
+    const toursRepository = this.defaultDataSource.getRepository(Tour);
     const existingTravels = await travelsRepository.find({
       take: 1,
     });
 
     if (existingTravels.length === 0) {
       await travelsRepository.save(
-        exampleTravels.map((travel) => travelsRepository.create(travel)),
+        exampleTravels.map((travel) => travelsRepository.create({
+          ...travel,
+          tours: travel.tours.map((tour) => toursRepository.create(tour))
+        })),
       );
     }
   }
