59f7950ef4f65f56488610cafbb51533e710488e
Alexey Furs <wiemrid@gmail.com>
2024-01-17 00:34:33 +0300

    change port, added cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,10 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+  await app.listen(8000);
 }
 bootstrap();
