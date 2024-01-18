0250f4342e36578c938d6cac3071767f4189aa20
Alexey Furs <wiemrid@gmail.com>
2024-01-18 14:44:00 +0300

    fix: roles query is not available

--- backend/src/roles/roles.module.ts
+++ backend/src/roles/roles.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RolesService } from './services';
 import { Role } from './entities';
+import { RolesResolver } from './resolvers';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
-  providers: [RolesService],
+  providers: [RolesService, RolesResolver],
   exports: [RolesService],
 })
 export class RolesModule {}
